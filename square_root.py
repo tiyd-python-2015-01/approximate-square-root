3ca24f850bfd6b42156b2c7ed61083c664aee489
Alan Grissett <alan.grissett@gmail.com>
2015-01-12 17:08:41 -0500

    Finished homework

--- square_root.py
+++ square_root.py
@@ -1,18 +1,17 @@
 def square_root(user_input):
-
-    if user_input == 1:
-        return 1
-    elif user_input < 0:
+    if user_input < 0:
         return complex(0, newtons_formula(user_input * -1))
     else:
         return newtons_formula(user_input)
+
 
 def newtons_formula(target):
     next_guess = target * .9
     epsilon = 0.01
     count = 1
 
-    while (next_guess ** 2 - target) > epsilon:
+    while (next_guess ** 2 - target) > epsilon or (
+          (next_guess ** 2 - target) < (epsilon * -1)):
         next_guess = (next_guess + (target / next_guess)) / 2
         print("Iteration: {}".format(count))
         print("Current Guess: {}\n".format(next_guess))
