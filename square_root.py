e5321161305ee352dda15fe9fdac8859a9240bc6
Gordon Fierce <gordon@Gordons-MacBook-Air.local>
2015-01-12 16:57:36 -0500

    Upgraded to harder homework

--- square_root.py
+++ square_root.py
@@ -1,15 +1,26 @@
 epsilon = 0.01
 
-user_input = input("Enter a positive number: ")
+while True:
 
-try:
-    number = float(user_input)
-except ValueError:
-    print("Not a valid number.")
+    user_input = input("Enter a positive number: ")
+
+    try:
+        number = float(user_input)
+    except ValueError:
+        print("Not a valid number.")
+        continue
+    if number<0:
+        print("That's not a positive number.")
+    else:
+        break
+
 # Initialize a guess. What makes a good guess? Something closer than n-1, like n/2.
 guess = number/2
 # While loop until a guess variable is good enough.
+loop_pass = 1
 while (abs(pow(guess,2)-number)> epsilon):
     guess = (guess + (number/guess))/2
+    print("Pass number {}: {}".format(loop_pass, guess))
+    loop_pass += 1
 
 print("The square root of {} is {}".format(number, guess))
