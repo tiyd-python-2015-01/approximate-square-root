ea05c8a87a949604a940dd804ab3bb22056aa32f
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-01-12 18:24:59 -0500

    Added the extra hard part of the assignment

--- square_root.py
+++ square_root.py
@@ -2,21 +2,25 @@ from random import randint
 
 while True:
     try:
-        num = int(input("Enter a Positive Number: "))
-        if num >0:
+        value_read = int(input("Enter an Integer Number: "))
+        if value_read > 0:
+            imaginary=' '
+            num=value_read
             break
         else: 
-            print("Oops!  That number is not positive.  Try again...")
+            imaginary='i'
+            num=abs(value_read)
+            break
     except ValueError: 
-        print("Oops!  That was not a number.  Try again...")
+        print("Oops!  That was not an integer number.  Try again...")
 
-x0=randint(1,num)
-i=1
+x0 = randint(1,num)
+i = 1
 while True:
-   print("This is iteration ",i,"and my current guess is", x0)
-   x1=(x0+(num/x0))/2
-   if (abs(x0-x1) < 0.01):
+    print("This is iteration ",i,"and my current guess is", x0)
+    x1 = (x0 + (num / x0))/2
+    if (abs(x0-x1) < 0.01):
         break
-   x0=x1
-   i+=1
-print("The square root of",num,"is",x0)
+    x0 = x1
+    i += 1
+print("The square root of",value_read,"is",round(x0,2),imaginary)
