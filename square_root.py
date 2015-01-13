457f033de92e106818d1f7e782d34580333bdeda
Gordon Fierce <gordon@Gordons-MacBook-Air.local>
2015-01-13 09:00:34 -0500

    Got complex numbers online

--- square_root.py
+++ square_root.py
@@ -2,25 +2,33 @@ epsilon = 0.01
 
 while True:
 
-    user_input = input("Enter a positive number: ")
+    user_input = input("Enter a number: ")
 
     try:
         number = float(user_input)
+        break
     except ValueError:
         print("Not a valid number.")
-        continue
-    if number<0:
-        print("That's not a positive number.")
-    else:
-        break
+
 
 # Initialize a guess. What makes a good guess? Something closer than n-1, like n/2.
-guess = number/2
+
 # While loop until a guess variable is good enough.
-loop_pass = 1
-while (abs(pow(guess,2)-number)> epsilon):
-    guess = (guess + (number/guess))/2
-    print("Pass number {}: {}".format(loop_pass, guess))
-    loop_pass += 1
+def find_root(my_num,precision):
+    guess = my_num/2
+    loop_pass = 1
+    while (pow(guess,2)-my_num)>precision:
+        guess = (guess + (my_num/guess))/2
+        print("Pass number {}: {}".format(loop_pass, guess))
+        loop_pass += 1
+    return guess
+
+if number<0:
+    pos_number = -1*number
+    positive_root = find_root(pos_number, epsilon)
+    guess = complex(positive_root, 1)
+
+else:
+    guess = find_root(number, epsilon)
 
 print("The square root of {} is {}".format(number, guess))
