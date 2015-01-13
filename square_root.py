fb478701525aab52e23948b28945e836671a2fca
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-01-12 19:16:26 -0500

    Changes made in order to work with pep8.

--- square_root.py
+++ square_root.py
@@ -1,11 +1,12 @@
 squared_number = float(input("Please enter a positive number you "
                              "would like to find the square root of: "))
-guess_number = 0.01
+guess_number = 1
+error = 0.0001
 iteration = 1
 
 
-while not (guess_number**2 - 0.0001) < squared_number < (guess_number**2 + 0.00001):
-    print("This is iteration {}".format(iteration))
+while not guess_number**2 - error < squared_number < guess_number**2 + error:
+    print("This is iteration", iteration)
     iteration += 1
     print("The number used to guess was", guess_number)
     average_number = (guess_number + (squared_number/guess_number))/2
